878f3250b6f669fb885cfeb54b1baaf4e488132a
pheldu95 <phelps.duncan@gmail.com>
2025-05-20 15:44:13 -0500

    setup db connection. By creating AppDbContext and adding it as a service

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27f96adfbe6e9360c64344d311579c81b862bf4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea173702ea3c86af8ffdbd777138de41b9f3ab4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
