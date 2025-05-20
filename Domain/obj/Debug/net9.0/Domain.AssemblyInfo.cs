ea173702ea3c86af8ffdbd777138de41b9f3ab4f
pheldu95 <phelps.duncan@gmail.com>
2025-05-20 12:50:43 -0500

    created first entity class. Activity.cs

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8778ed91b76c54202017c1b722925fc383b7081")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27f96adfbe6e9360c64344d311579c81b862bf4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
